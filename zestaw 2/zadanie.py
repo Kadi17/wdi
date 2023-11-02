9a90ef196ed0986c04dd914cb5dd6d915b7b9145
Kadi17 <kapmid123@gmail.com>
2023-11-02 12:30:57 +0100

    zadania 08.11

---
+++ zestaw 2/zadanie.py
@@ -0,0 +1,18 @@
+string = input('podaj ciąg znaków: ')
+#print(len(string))
+x = 0
+evenN = 1
+list = []
+while x < len(string):
+    if evenN % 2 == 0 and string[x] != '0':
+        list.append(string[x])
+    if string[x] == '0':
+        evenN = evenN +1
+    x = x+1
+ascii = []
+print(list)
+for letter in list:
+    ascii.append((ord(letter)))
+ascii.sort()
+print(ascii)
+print(ascii[4])
