5d635715bf1c437589abfb07e93e4c6909cf3288
Kadi17 <kapmid123@gmail.com>
2023-12-13 14:25:26 +0100

    '13.12.2023'

---
+++ zestaw 5/zadanie3.py
@@ -0,0 +1,27 @@
+array = [0, 4, 6, 8, 4, -20, 100, -500]
+def count_sequences(arr):
+    def is_arithmetic(a, b, c):
+        return 2 * b == a + c
+
+    def is_geometric(a, b, c):
+        return b * b == a * c
+
+    arithmetic_count = 0
+    geometric_count = 0
+
+    for i in range(len(arr) -2 ):
+        
+        if is_arithmetic(arr[i], arr[i + 1], arr[i + 2]):
+            arithmetic_count += 1
+
+        if is_geometric(arr[i], arr[i + 1], arr[i + 2]):
+            geometric_count += 1
+
+    if arithmetic_count > geometric_count:
+        return 1
+    elif arithmetic_count < geometric_count:
+        return -1
+    else:
+        return 0
+
+print(count_sequences(array))
